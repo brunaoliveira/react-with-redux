53be6f44aef35d0f1d13a7cc45676ae1f58b86cc
brunaoliveira <brunaol678@gmail.com>
2021-03-03 12:02:53 -0300

    initial template for the translate page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./styles.css";
 import React from "react";
 import Field from "./components/field";
-import languages from "./components/translate";
-import translate from "./components/translate";
+import Languages from "./components/languages";
+import Translate from "./components/translate";
 
 export default function App() {
   return (
     <div>
-      <h1 className="title">Hi there!</h1>
+      <Field />
+      <Languages />
+      <hr />
+      <Translate />
     </div>
   );
 }
