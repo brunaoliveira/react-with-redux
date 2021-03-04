d2e9d739b7d6e316796953992296b48aebb217c8
brunaoliveira <brunaol678@gmail.com>
2021-03-03 21:46:04 -0300

    create react component

---
+++ jsx/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// if (module.hot) {
+//     module.hot.accept();
+// }
+
+const App = function() {
+    return <div>Hi there!</div>
+};
