c1febdffec82695f361f13312bb86e6d3757fe8f
brunaoliveira <brunaol678@gmail.com>
2021-03-06 08:41:07 -0300

    use faker to generate data

--- components/src/index.js
+++ components/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     if (module.hot) {
         module.hot.accept();
     }
-
+    console.log(faker.name.findName());
     return (
         <div className="ui container comments">
-            <CommentDetail author="Blair"/>
-            <CommentDetail author="Charlie"/>
-            <CommentDetail author="Taylor"/>
+            <CommentDetail 
+                author={ faker.name.findName() } 
+                timeAgo={ faker.date.recent().toLocaleString() } 
+                content={ faker.lorem.text() } />
+            <CommentDetail author={ faker.name.findName() } timeAgo={ faker.date.recent().toLocaleString() } content={ faker.lorem.text() } />
+            <CommentDetail author={ faker.name.findName() } timeAgo={ faker.date.recent().toLocaleString() } content={ faker.lorem.text() } />
         </div>
     );
 };
