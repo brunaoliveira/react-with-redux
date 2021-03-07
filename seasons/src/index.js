e41d53c3019531dfb473aa0d50a7ce80c842d8b5
brunaoliveira <brunaol678@gmail.com>
2021-03-07 09:32:31 -0300

    render error message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -26,6 +26,7 @@ class App extends React.Component {
         return (
             <div>
                 Latitude: {this.state.lat}
+                Error: {this.state.errorMessage}
             </div>
         )
     }
